8a19496a6121b7d6df78bfe2cfc90befc90215a1
Drekstord <113056373+Drekstord@users.noreply.github.com>
2023-12-05 17:30:50 +0100

    Etape 2 fonctionne
    Mais etape 2 + etape 1 pas encore fonctionnel

--- compter.py
+++ compter.py
@@ -1,4 +1,4 @@
-def compterDesMots(untexte):
+def compter_des_mots(untexte):
     mondico = {}
     mots = untexte.split()
     for mot in mots:
@@ -7,7 +7,7 @@ def compterDesMots(untexte):
         else:
             mondico[mot] = 1
 
-    trie = sorted(mondico, key=lambda mot: mondico[mot], reverse=True)
+    trie = sorted(mondico, key=lambda word: mondico[word], reverse=True)
     dico_trie = {}
     for mot in trie:
         dico_trie[mot] = mondico[mot]
@@ -15,5 +15,21 @@ def compterDesMots(untexte):
     return dico_trie
 
 
+with open('mots_parasites.csv', 'r') as fichier:
+    parasite = fichier.read()
+    liste_parasite = parasite.splitlines()
+
+
+def suppression_parasite(dico_trie, liste_parasite):
+    liste_utile = []
+    for mot in dico_trie:
+        if mot not in liste_parasite:
+            liste_utile.append(mot)
+    return liste_utile
+
+
 letexte = input("Saisissez un texte : ")
-print(compterDesMots(letexte))
+dico_trie = compter_des_mots(letexte)
+resultat_liste_utile = suppression_parasite(dico_trie, liste_parasite)
+print(compter_des_mots(letexte))
+print(resultat_liste_utile)
